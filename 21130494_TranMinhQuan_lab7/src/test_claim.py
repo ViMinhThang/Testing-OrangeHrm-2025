85c393cece948798f69eaf3055ce9a72fff3dc4b
Trần Minh Quân <21130494@st.hcmuaf.edu.vn>
2025-11-28 13:34:55 +0700

    Add more test case

--- 21130494_TranMinhQuan_lab7/src/test_claim.py
+++ 21130494_TranMinhQuan_lab7/src/test_claim.py
@@ -6,6 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 
+def wait_and_click(driver, by, locator, time=7):
+    WebDriverWait(driver, time).until(
+        EC.visibility_of_element_located((by, locator))
+    )
+    driver.find_element(by, localtor).click()
+
 def test_TC_Claim_OK(driver):
     # Chuyển qua trang Claim
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/claim/viewAssignClaim")
@@ -92,6 +98,85 @@ def test_TC_Claim_EmployeeName_Missing(driver):
             EC.presence_of_element_located((By.XPATH, '//span[text()="Invalid"]'))
         )
         assert True
+
+    except TimeoutException:
+        assert False
+        
+def test_TC_Claim_Attributes_Missing(driver):
+    # Chuyển qua trang Claim
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/claim/viewAssignClaim")
+    # Đợi web hiện lên
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "h6.oxd-topbar-header-breadcrumb-module"))
+    )
+    
+    # Nhấn nút "Assign Claim"
+    button = driver.find_element(By.CSS_SELECTOR, "div.orangehrm-header-container button")
+    button.click()
+    # Đợi web hiện lên
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "h6.oxd-topbar-header-breadcrumb-module"))
+    )
+    # Nhập tên nhân viên
+    employee_name_input = driver.find_element(By.XPATH, "//input[@placeholder='Type for hints...']")
+    employee_name_input.send_keys("Ranga")
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.XPATH, "//*[text()='Ranga  Akunuri']"))
+    )
+    driver.find_element(By.XPATH, "//*[text()='Ranga  Akunuri']").click()
+    # Submit
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button.click()
+    
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//span[text()="Required"]'))
+        )
+        assert True
+
+    except TimeoutException:
+        assert False
+        
+def test_TC_Claim_Myself_OK(driver):
+    # Chuyển qua trang Claim
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/claim/viewAssignClaim")
+    # Đợi web hiện lên
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "h6.oxd-topbar-header-breadcrumb-module"))
+    )
+    # Chuyển qua My Claims
+    driver.find_element(By.XPATH, "//*[text()='My Claims']").click()
+    # Đợi
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.XPATH, "//*[text()='Submit Claim']"))
+    )
+    # Nhấn nút "Submit Claim"
+    button = driver.find_element(By.XPATH, "//*[text()='Submit Claim']")
+    button.click()
+    # Đợi web hiện lên
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "h6.oxd-topbar-header-breadcrumb-module"))
+    )
+    # Chọn Event
+    comboboxes = driver.find_elements(By.CLASS_NAME, "oxd-select-text.oxd-select-text--active")
+    event_combobox = comboboxes[0]
+    event_combobox.click()
+    driver.find_element(By.XPATH, "//*[text()='Accommodation']").click()
+    # Chọn Currency
+    currency_combobox = comboboxes[1]
+    currency_combobox.click()
+    driver.find_element(By.XPATH, "//*[text()='Afghanistan Afghani']").click()
+    # Submit
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button.click()
+    
+    try:
+        # Chờ tối đa 15 giây xem URL có chứa chuỗi mong muốn không
+        WebDriverWait(driver, 10).until(
+            EC.url_contains("/claim/assignClaim/id")
+        )
+        # Nếu chạy đến đây nghĩa là không bị Timeout -> Test Pass
+        print("Test Passed: Đã chuyển hướng thành công.")
 
     except TimeoutException:
         assert False
