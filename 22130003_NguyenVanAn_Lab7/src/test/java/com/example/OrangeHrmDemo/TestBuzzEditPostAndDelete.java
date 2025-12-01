6594e6dacc9f4bbf9c742f64f71a26d625db6ea6
nguyenvanan040804 <22130003@st.hcmuaf.edu.vn>
2025-12-01 23:47:57 +0700

    ..

---
+++ 22130003_NguyenVanAn_Lab7/src/test/java/com/example/OrangeHrmDemo/TestBuzzEditPostAndDelete.java
@@ -0,0 +1,87 @@
+package com.example.OrangeHrmDemo;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+public class TestBuzzEditPostAndDelete {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testBuzzEditPostAndDelete() throws Exception {
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz");
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div/div[3]/div/div/div/div/div[2]/li/button/i")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div/div[3]/div/div/div/div/div[2]/li/ul/li[2]/p")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/div/div/div/form/div/div[2]/div/textarea")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/div/div/div/form/div/div[2]/div/textarea")).clear();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/div/div/div/form/div/div[2]/div/textarea")).sendKeys("xyz 111");
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/div/div/div/form/div[2]/div[2]/div/button/i")).click();
+    driver.findElement(By.cssSelector("input[type='file']")).clear();
+    driver.findElement(By.cssSelector("input[type='file']")).sendKeys("/Users/an/Downloads/anh-meo-cute-1.jpg");
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div[2]/div/div/div/form/div[3]/button")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div/div[3]/div/div/div/div/div[2]/li/button/i")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div/div[2]/div[2]/div/div/div/div[3]/div/div/div/div/div[2]/li/ul/li/p")).click();
+    driver.findElement(By.xpath("//div[@id='app']/div[3]/div/div/div/div[3]/button[2]")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
